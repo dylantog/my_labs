5edd4471bf2b9fb8dc8fc6ff380b292758525a6a
Dylan Torres <dylantorres538@gmail.com>
2026-03-15 11:24:16 -0600

    Creación del proyecto y solución del laboratorio 1

---
+++ Laboratorio1_C27796/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_C27796.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
